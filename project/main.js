3c63d225c3bb46e77d7caccb33a77b0fdc9e3f74
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2021-07-04 12:41:07 +0300

    Cleaning up

--- project/main.js
+++ project/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.130.0';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
