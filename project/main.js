9ec2aabe3dc50dc6f277e5e26b3c5c7406555873
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2021-07-08 13:33:00 +0300

    Add model and GLTF loader

--- project/main.js
+++ project/main.js
@@ -1,10 +1,15 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
+loader.load('../assets/scene.gltf', function (gltf) {
+  scene.add(gltf.scene);
+});
 
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
